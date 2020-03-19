2413ab823531fa4f9e627147a8f7e396ef7e4adc
Xandy Almeida <xandy_yt@hotmail.com>
2020-03-19 12:32:12 -0300

    1008.py

---
+++ INICIANTE/Python/1008.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+A = int(input(''))
+B = int(input(''))
+C = float(input(''))
+
+salary = B*C
+
+print('NUMBER = %i' %A)
+print('SALARY = U$ %.2f' %salary)
