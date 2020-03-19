1d0248785cbb8cc54d300594ba333042b48cceb3
Xandy Almeida <xandy_yt@hotmail.com>
2020-03-19 16:07:09 -0300

    1011.py

---
+++ INICIANTE/Python/1011.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+R = float(input(''))
+pi = 3.14159
+
+VOLUME = (4/3.0)*pi*R*R*R
+
+print('VOLUME = %.3f' %VOLUME)
