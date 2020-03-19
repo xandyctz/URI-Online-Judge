23d5d497f3a3fa60e45284d2406bda8bd1a118e6
Xandy Almeida <xandy_yt@hotmail.com>
2020-03-19 12:32:25 -0300

    1009.py

---
+++ INICIANTE/Python/1009.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+name = str(input(''))
+salary = float(input(''))
+sale = float(input(''))
+
+total = ((15*sale)/100)+salary
+
+print('TOTAL = R$ %.2f' %total)
