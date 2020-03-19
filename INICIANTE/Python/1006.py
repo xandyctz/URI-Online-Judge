5988aece8b30c10541461c94034d4fdc1b0484de
Xandy Almeida <xandy_yt@hotmail.com>
2020-03-19 11:37:46 -0300

    1006.py

---
+++ INICIANTE/Python/1006.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+A = float(input(''))
+B = float(input(''))
+C = float(input(''))
+
+MEDIA = ((A*2)+(B*3)+(C*5))/10
+
+print('MEDIA = %.1f' %MEDIA)
+
